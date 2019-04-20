b38840dab857e5d1798c058b0cda66c3d8945ffa
woojoo <woojoo@softmarshmallow.com>
2019-04-21 03:14:28 +0900

    Trash & Food

---
+++ Assets/Scripts/Trash/TrashBehaiviour.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(SpriteRenderer))]
+public class TrashBehaiviour : MonoBehaviour
+{
+	private SpriteRenderer spriteRenderer;
+	// Use this for initialization
+	void Start ()
+	{
+		spriteRenderer = GetComponent<SpriteRenderer>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	private float targetDragTime = 3f;
+	private float dragingTime = 0.1f;
+
+	private float dragTimePercent
+	{
+		get { return dragingTime / targetDragTime; }
+	}
+	private void OnMouseDrag()
+	{
+		if (targetDragTime > dragingTime)
+		{
+			dragingTime += Time.deltaTime;
+			Color tmp = spriteRenderer.color;
+			tmp.a = 1 - dragTimePercent;
+			spriteRenderer.color = tmp;
+		}
+		else
+		{
+			Destroy(this);
+		}
+	}
+}

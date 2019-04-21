a36abc17c718d9b4669bdce3c7393268de846690
woojoo <woojoo@softmarshmallow.com>
2019-04-21 09:04:14 +0900

    Cut scene

---
+++ Assets/Scripts/CutScene/CutSceneManager.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel.Design.Serialization;
+using UnityEngine;
+using UnityEngine.UIElements;
+using UnityEngine.UI;
+using Image = UnityEngine.UI.Image;
+
+public class CutSceneManager : MonoBehaviour
+{
+    public static CutSceneManager Instance;
+    public List<Sprite> datas;
+    public Image image;
+    private void Awake()
+    {
+        Instance = this;
+    }
+
+
+    public int currentCut = 0;
+    public void setCutScene(int i)
+    {
+        try
+        {
+            var sprite = datas[i];
+            image.sprite = sprite;
+//        _image.image = sprite;
+            currentCut = i;
+        }
+        catch (Exception e)
+        {
+
+        }
+
+    }
+
+    public void playNext()
+    {
+        currentCut += 1;
+        setCutScene(currentCut);
+    }
+
+    public void closePanel()
+    {
+        enabled = false;
+    }
+
+    public void openPanel()
+    {
+        enabled = true;
+    }
+
+}
---
+++ Assets/Scripts/Main/CutSceneData.cs
@@ -0,0 +1,12 @@
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CutSceneData: MonoBehaviour
+{
+
+    public List<Sprite> sprites;
+
+
+
+
+}
